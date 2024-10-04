2dad28b03c6ffaa1072172cb4e4d17d0280883d4
GithubKangMin <leelucky1@naver.com>
2024-10-04 17:25:04 +0900

    1주차 과제

--- umc_test/food.sql
+++ umc_test/food.sql
@@ -1,9 +1,10 @@
 create table food
 (
-    image_url  text        null,
-    created_at datetime(6) null,
-    updated_at datetime    null,
-    id         bigint auto_increment
-        primary key
+    image_url        text        null,
+    created_at       datetime(6) null,
+    updated_at       datetime    null,
+    id               bigint auto_increment
+        primary key,
+    user_prefer_food int         null
 );
 
--- umc_test/review.sql
+++ umc_test/review.sql
@@ -5,6 +5,9 @@ create table review
     id         int auto_increment
         primary key,
     created_at datetime      null,
-    updated_at datetime      null
+    updated_at datetime      null,
+    store_id   bigint        null,
+    constraint store_id
+        foreign key (store_id) references store (id)
 );
 
