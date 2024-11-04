7a4f212349601219a8c15ec53b321cbcd36cd47a
seoyeoneel02 <seoyeoneel02@gmail.com>
2024-11-05 03:39:16 +0900

    feat: 5주차 워크북 #9

---
+++ week5/domain/Terms.java
@@ -0,0 +1,31 @@
+package umc.spring.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import umc.spring.domain.common.BaseEntity;
+import umc.spring.domain.mapping.UserAgree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Terms extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String title;
+
+    private String content;
+
+    @Column(nullable = false)
+    private Boolean optional;
+
+    @OneToMany(mappedBy = "terms", cascade = CascadeType.ALL)
+    private List<UserAgree> userAgreeList = new ArrayList<>();
+}
